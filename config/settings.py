2c01341afeb5257c317373aecc0cdf65d0b52c93
aronjjacob <jacob.masecampo2004@gmail.com>
2025-09-27 17:33:43 +0800

    404 & 500 pages

--- config/settings.py
+++ config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9auz6gz!(o)traq67e6ka%(w!t^jl1pck5c5_3e3ktf+6tdr*l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
